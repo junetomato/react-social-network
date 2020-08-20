75ce4536041ac3513325a75e8cc86d5c585da7fc
nlikhopoi <junetomato@yandex.ru>
2020-08-20 12:33:34 +0800

    lesson 17 - amended props.likesCount

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -7,7 +7,7 @@ function Post( props ) {
             <img src="http://4.bp.blogspot.com/-Jx21kNqFSTU/UXemtqPhZCI/AAAAAAAAh74/BMGSzpU6F48/s1600/funny-cat-pictures-047-001.jpg" alt=""/>
             { props.message }
             <div>
-                <span>{ props.likes } likes</span>
+                <span>like</span> { props.likesCount }
             </div>
         </div>
     )
