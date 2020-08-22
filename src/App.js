90c8a38eedf0f0dad2e4346d90615fe341e936cf
nlikhopoi <junetomato@yandex.ru>
2020-08-22 20:38:27 +0800

    lesson 17 - created Dialogs component + move container to upper level

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react'
 import Header from './components/Header/Header.jsx'
 import Navbar from './components/Navbar/Navbar.jsx';
 import Profile from './components/Profile/Profile.jsx';
+import Dialogs from './components/Dialogs/Dialogs.jsx'
+
 import './App.css'
 
 function App() {
   return (
-    <div className="app-wrapper">
+    <div className='app-wrapper'>
       <Header />
       <Navbar />
-      <Profile />
+      <div className='app-wrapper-content'>
+        <Dialogs />
+        {/* <Profile /> */}
+      </div>
     </div>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts'
 
 function Profile() {
     return (
-        <div className={ s.content }>
+        <div>
             <div>
                 <img src="https://www.lopesan.com/blog/wp-content/uploads/2019/07/Bavaro-Beach.jpg" alt="React Samurai Course" />
             </div>
