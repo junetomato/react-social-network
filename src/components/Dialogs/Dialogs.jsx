67adb22d4f4bab8ac30659cd8b3183242838eb4b
nlikhopoi <junetomato@yandex.ru>
2020-09-02 20:55:08 +0800

    created layout for Dialogs component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,20 @@ import classes from './Dialogs.module.css'
 
 function Dialogs( props ) {
     return (
-        <div>
-            Dialogs
+        <div className={ classes.dialogs }>
+            <div className={ classes.dialogItems }>
+                <div className={ `${ classes.dialog } ${ classes.active }` }>Dmitrii</div>
+                <div className={ classes.dialog }>Andrey</div>
+                <div className={ classes.dialog }>Sveta</div>
+                <div className={ classes.dialog }>Sasha</div>
+                <div className={ classes.dialog }>Viktor</div>
+                <div className={ classes.dialog }>Valera</div>
+            </div>
+            <div className={ classes.messages }>
+                <div className={ classes.message }>Hi</div>
+                <div className={ classes.message }>How are you?</div>
+                <div className={ classes.message }>Bye</div>
+            </div>
         </div>
     )
 }
