94a5a1763c5f1ceed94ead818b634a12dcbd8e37
nlikhopoi <junetomato@yandex.ru>
2020-09-16 12:07:11 +0800

    lesson 30 - moved BrowserRouter to index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
@@ -11,23 +11,21 @@ import './App.css'
 
 function App( props ) {
     return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <Header />
-                <Navbar state={ props.state.sidebar } />
-                <div className='app-wrapper-content'>
-                    <Route path='/profile'
-                        render={ () => <Profile
-                            state={ props.state.profilePage } /> } />
-                    <Route path='/dialogs'
-                        render={ () => <Dialogs
-                            state={ props.state.dialogsPage } /> } />
-                    <Route path='/news' render={ () => <News /> } />
-                    <Route path='/music' render={ () => <Music /> } />
-                    <Route path='/settings' render={ () => <Settings /> } />
-                </div>
+        <div className='app-wrapper'>
+            <Header />
+            <Navbar state={ props.state.sidebar } />
+            <div className='app-wrapper-content'>
+                <Route path='/profile'
+                    render={ () => <Profile
+                        state={ props.state.profilePage } /> } />
+                <Route path='/dialogs'
+                    render={ () => <Dialogs
+                        state={ props.state.dialogsPage } /> } />
+                <Route path='/news' render={ () => <News /> } />
+                <Route path='/music' render={ () => <Music /> } />
+                <Route path='/settings' render={ () => <Settings /> } />
             </div>
-        </BrowserRouter>
+        </div>
     );
 }
 
