cb1974315d9a279f5b2f1350771fcf6cb64efa45
nlikhopoi <junetomato@yandex.ru>
2020-09-28 14:51:03 +0800

    lesson 33 - created render.js for re-render page when added new post

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,6 +10,7 @@ function MyPosts( props ) {
     let addPost = () => {
         let text = newPostElement.current.value
         props.addPost( text )
+        newPostElement.current.value = ''
     }
 
     return (
---
+++ src/render.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import { addPost } from './redux/state'
+
+let rerenderEntireTree = ( state ) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state={ state } addPost={ addPost } />
+            </BrowserRouter>
+        </React.StrictMode>,
+        document.getElementById( 'root' )
+    );
+}
+
+export default rerenderEntireTree
