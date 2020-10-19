d4b2fef35288da61b9b16a6e39dba2cbfc344e78
nlikhopoi <junetomato@yandex.ru>
2020-10-19 14:42:40 +0800

    lesson 42 - added redux to replace handmade store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css'
 import * as serviceWorker from './serviceWorker'
-import store from './redux/store'
+import store from './redux/redux-store'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
@@ -22,7 +22,9 @@ let rerenderEntireTree = ( state ) => {
 }
 
 rerenderEntireTree( store.getState() )
-store.subscribe( rerenderEntireTree )
+store.subscribe( () => {
+    rerenderEntireTree( store.getState() )
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/sidebar-reducer.js
+++ src/redux/sidebar-reducer.js
@@ -1,4 +1,12 @@
-function sidebarReducer( state, action ) {
+let initialState = {
+    friends: [
+        { id: 2, name: 'Andrey', avatar: 'https://avatarfiles.alphacoders.com/119/119747.jpg' },
+        { id: 3, name: 'Sveta', avatar: 'https://avatarfiles.alphacoders.com/119/119747.jpg' },
+        { id: 4, name: 'Sasha', avatar: 'https://avatarfiles.alphacoders.com/119/119747.jpg' },
+    ]
+}
+
+function sidebarReducer( state = initialState, action ) {
 
     return state
 }
